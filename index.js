b597a39711394c91d2e565571887afbc3b570ceb
AzothGuyx <cristianxapata@gmail.com>
2021-06-07 22:26:52 -0500

    [ADD]  useUnifiedTopology

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mongoose.set('useCreateIndex',true);
 
 
 mongoose
-.connect(MONGO_URI,{useNewUrlParser: true, useFindAndModify : false})
+.connect(MONGO_URI,{useNewUrlParser: true, useFindAndModify : false,useUnifiedTopology: true})
 .then(()=> server.start())
 .catch(console.log);
 
