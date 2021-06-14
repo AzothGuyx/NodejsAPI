dd918a15e35c42873c836e0860f2e555fe18ea7b
AzothGuyx <cristianxapata@gmail.com>
2021-06-14 15:34:25 -0500

    [ADD] test adds

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,30 @@
+module.exports={
+    user:{
+        _id: "60c57a45141059311c6c9fd8",
+        name: "Cristian",
+        username: "cherre",
+        password: "123456"
+    },
+
+    users:[
+        {
+            _id: "60c57a45141059311c6c9fd6",
+            name: "Cristian",
+            username: "cherre",
+            password: "123456"
+        },
+        {
+            _id: "60c57a45141059311c6c9fd7",
+            name: "Cristian",
+            username: "wilfri",
+            password: "123456"
+        },
+        {
+            _id: "60c57a45141059311c6c9fd9",
+            name: "Carlos",
+            username: "carlos",
+            password: "123456"
+        }
+
+    ]
+}
